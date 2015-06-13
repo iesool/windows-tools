1ac4d9cfb869bdfcdd128d0d82e364cb375161a9
Jiademing <iesool@126.com>
2015-06-13 15:59:03 +0800

    Add Finder defination.

---
+++ src/Finder.py
@@ -0,0 +1,32 @@
+#coding=utf-8
+'''
+Created on 20150613
+@author: Jiademing   iesool@126.com 
+'''
+
+class Finder:
+    __m_DiskDescrpts = ""
+    
+    def __init__(self):
+        pass
+    
+    def Find(self, dirname, key):
+        pass
+    
+    def FindAll(self, key):
+        pass
+    
+    def FindPictures(self, dirname, key):
+        pass
+    
+    def FindPicturesAll(self, key):
+        pass
+    
+    def FindVideos(self, dirname, key):
+        pass
+    
+    def FindVideosAll(self, key):
+        pass
+    
+    def __GetAllDescrpts(self):
+        pass
